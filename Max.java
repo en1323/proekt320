4b6bae42f387e4d94749e41aff34a87af3a57bf3
en1323 <62262689+en1323@users.noreply.github.com>
2026-02-11 12:41:06 +0500

    Create Max.java

---
+++ Max.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String[] args) {
+        int a = 67;
+        int b = 4;
+        int c = 34;
+        int j = 98;
+        int max =Math.max(a,b);
+        int max2=Math.max(c,j);
+        int max3=Math.max(max,max2);
+        System.out.println("Naibolshee chislo: " + max3);
+    }
+}
